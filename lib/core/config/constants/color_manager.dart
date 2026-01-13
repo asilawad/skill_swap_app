000421f89ec032211f8da42e9afd77cd2f18d611
Asil Awad <aseelawad8@hotmail.com>
2026-01-13 23:52:12 +0200

    add ColorManager constants

---
+++ lib/core/config/constants/color_manager.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  ColorManager._();
+
+  // Primary Colors
+  static const primaryBlue = Color(0xFF3E8FCC);
+  static const primaryBlueDark = Color(0xFF2F71A3);
+  static const primaryBlueDarker = Color(0xFF2F71A3);
+  static const primaryBlueMuted = Color(0xFF6680B3);
+
+  // State Colors
+  static const errorColor = Color(0xFFDC2626);
+  static const warningColor = Color(0xFFF59E0B);
+  static const successColor = Color(0xFF16A34A);
+  static const infoColor = Color(0xFF3B82F6);
+
+  // Text Colors
+  static const textPrimary = Color(0xFF0C0D0F);
+  static const textSecondary = Color(0xFF666666);
+  static const textHint = Color(0xFF9CA3AF);
+  static const textWhite = Color(0xFFFFFFFF);
+  static const textDisabled = Color(0xFF999999);
+
+  // Neutral Colors
+  static const backgroundPrimary = Color(0xFFF9FAFB);
+  static const backgroundWhite = Color(0xFFFFFFFF);
+  static const backgroundGrey = Color(0xFFE5E7EB);
+  static const backgroundBlueLight = Color(0xFFF7FAFF);
+
+  // Border & Divider Colors
+  static const borderLight = Color(0xFFF3F4F6);
+  static const borderGrey = Color(0xFFF5F5F5);
+}
