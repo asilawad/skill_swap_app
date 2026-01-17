d246b80d60c587b031a879205b65b9750ac35740
Asil Awad <aseelawad8@hotmail.com>
2026-01-17 17:01:11 +0200

    add NavigationExtension

---
+++ lib/core/config/router/router_extention.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+// How to use this?
+// context.navigateTo(RoutesName.login);
+
+extension NavigationExtension on BuildContext {
+  // Navigate to a new screen with a route name
+  Future navigateTo(String routeName, {Object? arguments}) {
+    return Navigator.pushNamed(this, routeName, arguments: arguments);
+  }
+
+  // Replace the current screen with a new one
+  Future navigateAndReplace(String routeName, {Object? arguments}) {
+    return Navigator.pushReplacementNamed(
+      this,
+      routeName,
+      arguments: arguments,
+    );
+  }
+
+  // Pop the current screen and return to the previous screen
+  void goBack<T extends Object?>([T? result]) {
+    Navigator.pop(this, result);
+  }
+
+  // Pop the current screen and return to the previous screen
+  void canGoBack<T extends Object?>() {
+    Navigator.maybePop(this);
+  }
+
+  // Navigate and remove all previous screens until a specific route is reached
+  Future navigateAndRemoveUntil(
+    String routeName,
+    RoutePredicate predicate, {
+    Object? arguments,
+  }) {
+    return Navigator.pushNamedAndRemoveUntil(
+      this,
+      routeName,
+      predicate,
+      arguments: arguments,
+    );
+  }
+
+  void popUntil(String routeName) {
+    return Navigator.popUntil(this, ModalRoute.withName(routeName));
+  }
+}
